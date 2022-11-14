ba285626ed626f537b9bd7a46d860d75930a0372
Luis Barca <luisbarcapons@gmail.com>
2022-11-14 18:55:21 +0100

    Update 14/11

--- agentMinMax.py
+++ agentMinMax.py
@@ -9,11 +9,13 @@ BOTAR = 6
 MOURE = 1
 
 
+
 class Estat:
-    def __init__(self, nom, pos_pizza, pos_agent, parets, pare=None):
+    def __init__(self, nom,  pos_pizza, pos_agent, parets, pare=None):
         self.__nom_agent = nom
-        self.__pos_pizza = pos_pizza
         self.__pos_agent = pos_agent
+        self.__nom_agent2 = list(pos_agent.keys())[1]
+        self.__pos_pizza = pos_pizza
         self.__parets = parets
         self.__pare = pare
 
@@ -67,22 +69,23 @@ class Estat:
             and (self.__pos_agent[self.__nom_agent][1] >= 0)
         )
 
-    def calcular_heuristica(self):
-        sum = 0
+    def calcular_heuristica(self, nom):
+        suma = 0
 
         for i in range(2):
-            sum += abs(self.__pos_pizza[i] - self.__pos_agent[self.__nom_agent][i])
+            suma += abs(self.__pos_pizza[i] - self.__pos_agent[nom][i])
+        
+        return suma
 
-        return sum
+    def calcular_puntuacio_agent(self):
+        agent2 = self.__nom_agent2
+        # Obtenim la puntuació de l'agent passat per paràmetre
+        if self.__nom_agent == agent2:
+            puntuacio = self.calcular_heuristica(self.__nom_agent) - self.calcular_heuristica(agent2)
+        else:
+            puntuacio = self.calcular_heuristica(agent2) - self.calcular_heuristica(self.__nom_agent)
 
-    def get_altre_agent(self):
-        agent = list(self.__pos_agent.keys())
-
-        for i in range(2):
-            if(self.__nom != agent[i]):
-                return agent[i]
-                
-        return None
+        return puntuacio
 
     def genera_fills(self):
         """Mètode que genera tot l'abre d'accions"""
@@ -102,15 +105,13 @@ class Estat:
         for i, m in enumerate(moviments.values()):
             coordenades = [sum(tup) for tup in zip(self.__pos_agent[self.__nom_agent], m)]
             moviment = {self.__nom_agent: coordenades}
-            agent2 = self.get_altre_agent()
 
             actual = Estat(
                 self.__nom_agent,
                 self.__pos_pizza,
                 moviment,
                 self.__parets,
-                cost,
-                (self, (AccionsRana.MOURE, Direccio.__getitem__(claus[i]))),
+                (self, (AccionsRana.MOURE, Direccio.__getitem__(claus[i])))
             )
 
             if actual.es_valid():
@@ -125,17 +126,15 @@ class Estat:
         }
 
         for i, m in enumerate(moviments.values()):
-            coordenades = [sum(tup) for tup in zip(self.__pos_agent["Luis"], m)]
-            moviment = {"Luis": coordenades}
-            cost = self.__pes + BOTAR
+            coordenades = [sum(tup) for tup in zip(self.__pos_agent[self.__nom_agent], m)]
+            moviment = {self.__nom_agent: coordenades}
 
             actual = Estat(
                 self.__nom_agent,
                 self.__pos_pizza,
                 moviment,
                 self.__parets,
-                cost,
-                (self, (AccionsRana.BOTAR, Direccio.__getitem__(claus[i]))),
+                (self, (AccionsRana.BOTAR, Direccio.__getitem__(claus[i])))
             )
 
             if actual.es_valid():
@@ -144,10 +143,8 @@ class Estat:
         return fills
 
     def evaluar(self):
-        if self.es_meta():
-            return self.__puntuacio
-
-        # Falta realitzar les puntuacions o comparacio
+        print(f"Ev {list(self.__pos_agent.keys())}")
+        return self.es_meta(), self.calcular_puntuacio_agent()
 
 class Rana(joc.Rana):
     def __init__(self, nom, *args, **kwargs):
@@ -155,22 +152,23 @@ class Rana(joc.Rana):
         self.__nom = self.nom
         self.__accions = None
         self.__botar = 0
-        self.__torn = 0
+        self.__meta = 0
 
-    def _cerca(self, estat: Estat, torn_max, profunditat):
+    def _cerca(self, estat: Estat, profunditat, torn_max=True):
         """Mètode que realitza la cerca del cami òptim per arribar a l'estat meta; utilitzant l'algorisme de cerca Min i Max"""
-        score = estat.evaluar()
+        meta, score = estat.evaluar()
+        if meta or profunditat == 3:
+            return score, estat
 
         puntuacio_fills = [
             self._cerca(estat_fill, not torn_max, profunditat + 1)
             for estat_fill in estat.genera_fills()
         ]
-        puntuacio = puntuacio_fills[0]
 
         if torn_max:
-            return max(puntuacio), estat
+            return max(puntuacio_fills)
         else:
-            return min(puntuacio), estat
+            return min(puntuacio_fills)
 
     def pinta(self, display):
         pass
@@ -178,34 +176,40 @@ class Rana(joc.Rana):
     def actua(
         self, percep: entorn.Percepcio
     ) -> entorn.Accio | tuple[entorn.Accio, object]:
+        
         percepcions = percep.to_dict()
         claus = list(percepcions.keys())
-        # percep[claus[0]] = pizza, percep[claus[1]] = rana, percep[claus[2]] = parets
+        # percep[claus[0]] = pizza, percep[claus[0, 1]] = rana, percep[claus[2]] = parets
         estat: Estat = Estat(
             self.__nom, percep[claus[0]], percep[claus[1]], percep[claus[2]]
         )
 
-        # Si no tenim accions, les cercam
-        if self.__accions is None:
-            self._cerca(estat=estat, torn_max=True)
-            print(self.__accions)
-
-        # Si tenim les accions
-        if self.__accions:
-            # Si acaba de botar, ha d'esperar
-            if self.__botar > 0:
-                self.__botar -= 1
-                return AccionsRana.ESPERAR
-
-            # Si ha de botar o si ha de mourer-se a una casella adjacent
-            else:
-                accio = self.__accions.pop()
-
-                if accio[0] == AccionsRana.BOTAR:
-                    self.__botar = 2
-
-                return accio[0], accio[1]
-
-        # Sino esperam
-        else:
+        actual = self._cerca(estat, 0)[1]
+        agent = percep[claus[1]].keys()
+        
+        for a in agent:
+            # Si l'agent està en la posició final
+            if percep[claus[1]][a] == percep[claus[0]]:
+                self.__meta = 1
+        
+        # Si qualque agent ha guanyat, no es mouen
+        if self.__meta == 1:
             return AccionsRana.ESPERAR
+
+        while actual.pare is not None:
+            pare, accio = actual.pare
+            actual = pare
+
+        # Si acaba de botar, ha d'esperar
+        if self.__botar > 0:
+            self.__botar -= 1
+            return AccionsRana.ESPERAR
+
+        # Si ha de botar o si ha de mourer-se a una casella adjacent
+        else:
+            accio = self.__accions.pop()
+
+            if accio[0] == AccionsRana.BOTAR:
+                self.__botar = 2
+
+            return accio[0], accio[1]
